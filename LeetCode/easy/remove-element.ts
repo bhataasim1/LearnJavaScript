9b0d4f2fa4ad2ba31dbf6b99be9a239dbbeb9425
Bhat Aasim <71691906+bhataasim1@users.noreply.github.com>
2025-01-12 19:10:36 +0530

    Create remove-element.ts

---
+++ LeetCode/easy/remove-element.ts
@@ -0,0 +1,15 @@
+
+//leet code:: https://leetcode.com/problems/remove-element/
+
+function removeElement(nums: number[], val: number): number {
+    let temp = 0;
+
+    for(let i = 0; i < nums.length; i++) {
+        if(nums[i] !== val){
+            nums[temp] = nums[i]
+            temp++
+        }
+    }
+
+    return temp;
+};
