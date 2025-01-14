a405ee87a06bddd6de96a1f6f68b9d4793d5ebbd
Bhat Aasim <71691906+bhataasim1@users.noreply.github.com>
2025-01-14 14:43:41 +0530

    Create plus-one.ts

---
+++ LeetCode/easy/plus-one.ts
@@ -0,0 +1,17 @@
+// leetcode:: https://leetcode.com/problems/plus-one/solutions/6278309/easy-solution-in-typescript/
+
+function plusOne(digits: number[]): number[] {
+
+    for (let i = digits.length - 1; i >= 0; i--) {
+        if (digits[i] < 9) {
+            digits[i] += 1;
+            return digits;
+        }
+        digits[i] = 0;
+    }
+    
+    const newDigits = new Array(digits.length + 1).fill(0);
+    newDigits[0] = 1;
+
+    return newDigits;
+};
