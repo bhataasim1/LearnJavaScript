f38e46aadb0aaee6d0be406a6b0452372f1c0192
Bhat Aasim <71691906+bhataasim1@users.noreply.github.com>
2025-01-17 15:24:25 +0530

    Create remove-duplicates-from-list.ts

---
+++ LeetCode/easy/remove-duplicates-from-list.ts
@@ -0,0 +1,29 @@
+// leet code: https://leetcode.com/problems/remove-duplicates-from-sorted-list/description/
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function deleteDuplicates(head: ListNode | null): ListNode | null {
+    let current: ListNode = head;
+
+    if(head == null) return head
+
+    while(current.next !== null) {
+        if(current.val === current.next.val) {
+            current.next = current.next.next;
+        } else {
+            current = current.next;
+        }
+
+    }
+    return head;
+};
